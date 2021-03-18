8e409985e257a5284c26fa8caf3b37ce98cdf695
Angel Burgos Desktop <angelpublicista.1@gmail.com>
2021-03-17 19:46:49 -0500

    fin widget 1 agenda

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.oc-slick-calendar').slick({
+        slidesToShow: 1,
+        prevArrow: "<img src='../img/slick-arrow-left.svg' class='custom-slick-arrow arrow-left'>",
+        nextArrow: "<img src='../img/slick-arrow-right.svg' class='custom-slick-arrow arrow-right'>"
+    })
+})
